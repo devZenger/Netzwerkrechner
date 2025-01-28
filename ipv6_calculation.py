81cfc3d470950e1391cacdb36469a1c4832fb826
devZenger <alwayscarryatowel@mail.de>
2025-01-28 08:59:41 +0100

    Add ipv6 cidir input check

--- ipv6_calculation.py
+++ ipv6_calculation.py
@@ -43,6 +43,8 @@ def add_zero_to_ipv6_input(input):
 class IPv6Page(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
+        
+        default_setting = {'padx':(20,5), 'pady': 5, 'sticky': "ew"}
 
         label = tk.Label(self, text="IPv6:", font=(14))
         label.grid(row=0, column=0, pady=10)
@@ -54,14 +56,39 @@ class IPv6Page(tk.Frame):
 
         cidir_label = tk.Label(self, text="CIDIR: ")
         cidir_label.grid(row=3, column=0)
-        cidir_input = tk.Entry(self)
-        cidir_input.grid(row=3, column=1)
+        self.cidir_input = tk.Entry(self)
+        self.cidir_input.grid(row=3, column=1)
 
         cal_ipv6 = tk.Button(self, text="berechnen", command=self.ipv6_cal)
         cal_ipv6.grid(row=3, column=2)
 
         self.error_output = tk.Label(self, text="")
         self.error_output.grid(row=4, column=1)
+        
+        ipv6_output_label = tk.Label(self, text="IPv6 Adresse: ", anchor="w")
+        ipv6_output_label.grid(**default_setting, row=5, column=0)
+        self.ipv6_output = tk.Label(self, text="")
+        self.ipv6_output.grid(row=5, column=1)
+
+        subnet_mask_output_label = tk.Label(self, text="Subnetzmaske: ", anchor="w")
+        subnet_mask_output_label.grid(**default_setting, row=6, column=0)
+        self.subnet_mask_output = tk.Label(self, text="")
+        self.subnet_mask_output.grid(row=6, column=1)
+
+        wildcard_mask_output_label = tk.Label(self, text="Wildcard-Maske: ", anchor="w")
+        wildcard_mask_output_label.grid(**default_setting, row=7, column=0)
+        self.wildcard_mask_output = tk.Label(self, text="")
+        self.wildcard_mask_output.grid(row=7, column=1)
+
+        net_id_output_label = tk.Label(self, text="Netzwerkadresse: ", anchor="w")
+        net_id_output_label.grid(**default_setting, row=8, column=0)
+        self.net_id_output = tk.Label(self, text="")
+        self.net_id_output.grid(row=8, column=1)
+
+        broadcast_output_label = tk.Label(self, text="Broadcast-Adresse:", anchor="w")
+        broadcast_output_label.grid(**default_setting, row=9, column=0)
+        self.broadcast_output = tk.Label(self, text="")
+        self.broadcast_output.grid(row=9, column=1)
 
 
     # ipv6 calculation
@@ -81,8 +108,18 @@ class IPv6Page(tk.Frame):
             self.error_output.config(text="Fehlerhafte Eingabe")
 
         if len(ipv6_adresses) != 8:
+            print(len(ipv6_adresses))
             self.error_output.config(text="Fehlerhafte Eingabe")
 
+        try:
+            cidir = int(self.cidir_input.get())
+        except:
+            self.error_output.config(text="Fehlerhafte Eingabe")
+            
+
+        if cidir < 0 or cidir > 128 :
+            self.error_output.config(text="Fehlerhafte Eingabe")
+        
         for i in ipv6_adresses:
             print(i, end="  ")
 
